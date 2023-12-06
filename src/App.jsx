63a9f13f548350502d9ca46d86cc7c65ef3e555a
Nikilesh Raj <nikileshnatraj@gmail.com>
2023-12-06 23:16:19 +0530

    modified app jsx to include image from import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-function AppHeader() {
+import reactImage from './assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {description} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
@@ -14,7 +24,7 @@ function AppHeader() {
 function App() {
   return (
     <div>
-      <AppHeader />
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
