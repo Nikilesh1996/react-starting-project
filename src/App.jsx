f34a7d130300a8353e7e1cb9ee00a4c1927e3c2c
Nikilesh Raj <nikileshnatraj@gmail.com>
2023-12-13 01:25:09 +0530

    Added data.js and reusable components
    Modified app js to accomodate the same

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import reactImage from './assets/react-core-concepts.png';
+import reactImage from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
-
   const description = reactDescriptions[genRandomInt(2)];
 
   return (
@@ -15,9 +15,35 @@ function Header() {
       <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description} React concepts you will need for almost any app you are going to build!
+        {description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
+  );
+}
+
+/*
+// Alternate declaration of the component.
+function CoreConcept(props) {
+  const { title, description } = props;
+
+  return (
+    <li>
+      <img src={props.image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+*/
+
+function CoreConcept({ title, description, image }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
   );
 }
 
@@ -26,6 +52,19 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
