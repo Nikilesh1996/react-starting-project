4bd53157f2276051790ba9fced20d938c1d28def
Nikilesh Raj <nikileshnatraj@gmail.com>
2023-12-14 23:39:46 +0530

    Modified app function component to display array of components with map function

--- src/App.jsx
+++ src/App.jsx
@@ -34,14 +34,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts {...CORE_CONCEPTS[0]} />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+            {CORE_CONCEPTS.map((coreConcept) => (
+              <CoreConcepts key={coreConcept.title} {...coreConcept} />
+            ))}
           </ul>
         </section>
 
