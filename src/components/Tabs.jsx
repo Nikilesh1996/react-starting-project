b3452966d381db58f5d8d4beea4c5e74fc5b386c
Nikilesh Raj <nikileshnatraj@gmail.com>
2023-12-16 00:39:35 +0530

    Modified to accept component as string

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,12 @@
-export default function Tabs({ buttons, children }) {
+export default function Tabs({ buttons, children, buttonsContainer }) {
+  // React somehow treats this as a component.
+  // This is why custom components should always 
+  // start with upper case character.
+  const ButtonsContainer = buttonsContainer;
+
   return (
     <>
-      <menu>{buttons}</menu>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
       {children}
     </>
   );
